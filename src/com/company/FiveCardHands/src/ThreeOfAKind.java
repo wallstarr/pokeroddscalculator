c9f44608f2bcf10fbce2061c13dc8e540610b19a
Dan Blustein <dansakai0@gmail.com>
2022-05-01 18:37:45 -0700

    fixes for three of a kind

--- src/com/company/FiveCardHands/src/ThreeOfAKind.java
+++ src/com/company/FiveCardHands/src/ThreeOfAKind.java
@@ -8,11 +8,15 @@ public class ThreeOfAKind extends FiveCardHand {
     public static boolean matches(Card[] fiveCardHand) {
         Card[] sortedFiveCardHandAscending = getSortedFiveCardsAscending(fiveCardHand);
         boolean firstThreeAreSameRank = fiveCardHand[0].getRank() == fiveCardHand[1].getRank() &&
-                fiveCardHand[1].getRank() == fiveCardHand[2].getRank();
+                fiveCardHand[1].getRank() == fiveCardHand[2].getRank() &&
+                fiveCardHand[2].getRank() != fiveCardHand[3].getRank();
         boolean middleThreeAreSameRank = fiveCardHand[1].getRank() == fiveCardHand[2].getRank() &&
-                fiveCardHand[2].getRank() == fiveCardHand[3].getRank();
+                fiveCardHand[2].getRank() == fiveCardHand[3].getRank() &&
+                fiveCardHand[3].getRank() != fiveCardHand[4].getRank() &&
+                fiveCardHand[0].getRank() != fiveCardHand[1].getRank();
         boolean lastThreeAreSameRank = fiveCardHand[2].getRank() == fiveCardHand[3].getRank() &&
-                fiveCardHand[3].getRank() == fiveCardHand[4].getRank();
+                fiveCardHand[3].getRank() == fiveCardHand[4].getRank() &&
+                fiveCardHand[1].getRank() != fiveCardHand[2].getRank();
         return firstThreeAreSameRank || middleThreeAreSameRank || lastThreeAreSameRank;
     }
 }
